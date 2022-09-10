258a3f6ab011b8f16e3ac61ae4e7795d6f063631
yashsilveroak07 <113232039+yashsilveroak07@users.noreply.github.com>
2022-09-10 12:59:34 +0530

    Add files via upload

---
+++ yash.sql
@@ -0,0 +1,19 @@
+create table Stu_Detail
+(S_no number(5), S_Name varchar(30), S_School varchar(50), S_Hobby varchar(20), S_std number(5), S_Gender varchar(6));
+
+insert into Stu_Detail values (1,'Manav','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (2,null,'Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (3,'Yash .p','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (4,'Yash .s','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (5,'Prem','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (6,'Heer','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (7,'Axit','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (8,'Manthan','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (9,'Dixit','Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (10,null,'Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (11,null,'Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (12,null,'Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (13,null,'Earth_English_School','Gaming',12,'male');
+insert into Stu_Detail values (14,null,'Earth_English_School','Gaming',12,'male');
+
+select * from Stu_Detail
